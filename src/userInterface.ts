7cdb604ac9ea3efbd0e808d70e7020cb6ef57680
bharath <bharath.hj@surya-soft.com>
2022-04-07 10:27:23 +0530

    Calculator TS
    - added type script files ( functionality part of calculator )

---
+++ src/userInterface.ts
@@ -0,0 +1,8 @@
+export interface webpageElements {
+  equationField: HTMLInputElement;
+  answerField: HTMLInputElement;
+  acButton: Element;
+  numButtons: NodeListOf<Element>;
+  operationButtons: NodeListOf<Element>;
+  backButton: Element;
+}
