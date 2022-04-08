cae22f616997fe1e6c235825c2119e3dc91185d0
bharath <bharath.hj@surya-soft.com>
2022-04-08 07:11:23 +0530

    Code Refactored in Type Script File
    - removed NumPadEvents and Operation Events
    - added InputEvents and OutputEvents
    - modification doone in the Calculation Part of Calculator App
    - id names are refactored
    - font spacing and padding are added
    - font size is refactored

--- src/function.ts
+++ src/function.ts
@@ -1,114 +1,139 @@
-export const acButtonEvent = (
-  acButton: Element,
-  equationField: HTMLInputElement,
-  answerField: HTMLInputElement
+//Global Values
+const operationDictionary: Object = {
+  addition: '+',
+  subtraction: '-',
+  modulus: '%',
+  multiplication: '*',
+  division: '/',
+  point: '.',
+};
+
+let operationSymbols = Object.values(operationDictionary);
+
+const equationOriginalFontSize = 60;
+const ansOriginalFontSize = 45;
+const maxDigitBeforeSizeChange = 8;
+const percentageDecrease = 0.1;
+
+let equationFontSizeChange = equationOriginalFontSize;
+let ansFontSizeChange = ansOriginalFontSize;
+
+// All Clear Function
+export const allClearButtonEvent = (
+  AC: Element,
+  inputScreen: HTMLInputElement,
+  outputScreen: HTMLInputElement
 ): void => {
-  acButton.addEventListener('click', () => {
-      equationField.value = '';
-      answerField.value = '';
-      input = ['', ''];
-      answer = '';
-      numEquation = 0;
-      operation = '';
-      resetFontSize(equationField, answerField);
+  AC.addEventListener('click', () => {
+    inputScreen.value = '';
+    outputScreen.value = '';
   });
 };
 
-export const numPadEvents = (
-  numButtons: NodeListOf<Element>,
-  equationField: HTMLInputElement
+// Input Screen Function
+export const inputEvent = (
+  numberButtons: NodeListOf<Element>,
+  inputScreen: HTMLInputElement,
+  operationButtons: NodeListOf<Element>,
+  outputScreen: HTMLInputElement
 ): void => {
-  for (let numButton of numButtons) {
-      numButton.addEventListener('click', () => {
-          if (numButton.id === 'decimal') {
-              //making sure that a number does not have more than 1 decimal
-              if (input[numEquation].indexOf('.') === -1) {
-                  input[numEquation] += '.';
-              }
-          } else {
-              input[numEquation] += numButton.id[3];
-          }
-
-          equationField.value = input[numEquation];
-
-          //Decrease the font size depending on the number of digits
-          equationFontSizeChange = checkDecreaseFontSize(
-              input[numEquation],
-              equationOriginalFontSize
-          );
-
-          equationField.style.fontSize = `${equationFontSizeChange}px`;
-      });
+  for (let numberButton of numberButtons) {
+    // Number Input
+    numberButton.addEventListener('click', () => {
+      inputScreen.value += numberButton.id;
+      // Font Size Change
+      equationFontSizeChange = checkDecreaseFontSize(
+        inputScreen.value,
+        equationOriginalFontSize
+      );
+      inputScreen.style.fontSize = `${equationFontSizeChange}px`;
+    });
+  }
+  // Operator Input
+  for (let operationButton of operationButtons) {
+    operationButton.addEventListener('click', () => {
+      if (operationButton.id !== 'equals') {
+        let temp: string = inputScreen.value;
+        inputScreen.value += operationDictionary[`${operationButton.id}`];
+        inputScreen.value = replaceOperatorCheck(inputScreen.value);
+      }
+    });
   }
 };
 
-export const operationEvents = (
+// Output Screen Function
+export const outputEvent = (
+  inputScreen: HTMLInputElement,
   operationButtons: NodeListOf<Element>,
-  equationField: HTMLInputElement,
-  answerField: HTMLInputElement
+  outputScreen: HTMLInputElement
 ): void => {
   for (let operationButton of operationButtons) {
-      operationButton.addEventListener('click', () => {
-          if (numEquation && operationButton.id !== 'equals') {
-              input[0] = evaluate(input[0], input[1], operation);
-              equationField.value = input[0];
-              input[1] = '';
-          }
-
-          if (operationButton.id !== 'equals') {
-              numEquation = 1;
-              operation = operationButton.id;
-              equationField.value = '';
-              answerField.value = input[0];
-              resetFontSize(equationField, answerField);
-          } else {
-              displayResults(equationField, answerField);
-          }
-      });
+    operationButton.addEventListener('click', () => {
+      if (operationButton.id === 'equals') {
+        let expression: string = inputScreen.value;
+        // Check if any operator is last Character
+        if (
+          operationSymbols.indexOf(`${expression[expression.length - 1]}`) !==
+          -1
+        ) {
+          return;
+        }
+        // Calculation of the equation
+        let equation: string = inputScreen.value;
+        outputScreen.value = eval(equation);
+        // Font Size Change
+        ansFontSizeChange = checkDecreaseFontSize(
+          outputScreen.value,
+          ansOriginalFontSize
+        );
+        outputScreen.style.fontSize = `${ansFontSizeChange}px`;
+      }
+    });
   }
 };
 
+// Delete a Character
 export const backspaceEvent = (
   backButton: Element,
-  equationField: HTMLInputElement
+  inputScreen: HTMLInputElement
 ): void => {
   backButton.addEventListener('click', () => {
-      //This logic here is to slice the string from starting index
-      //to the length of string - 1
-      //to remove the very last character from the string
-      input[numEquation] = input[numEquation].slice(
-          0,
-          input[numEquation].length - 1
-      );
-
-      //If the string is already empty then change it's value to '0'
-      //instead of slicing it further
-      if (input[numEquation] === '') {
-          input[numEquation] = '0';
-      }
-
-      equationField.value = input[numEquation];
+    inputScreen.value = inputScreen.value.slice(
+      0,
+      inputScreen.value.length - 1
+    );
   });
 };
 
-const resetFontSize = (
-  equationField: HTMLInputElement,
-  answerField: HTMLInputElement
-): void => {
-  equationFontSizeChange = equationOriginalFontSize;
-  equationField.style.fontSize = `${equationFontSizeChange}px`;
-  ansFontSizeChange = ansOriginalFontSize;
-  answerField.style.fontSize = `${ansOriginalFontSize}px`;
+// Replace the old Operator with new Operator
+const replaceOperatorCheck = (temp: string): string => {
+  let len: number = temp.length;
+  if (len > 1) {
+    if (
+      operationSymbols.indexOf(`${temp[len - 2]}`) !== -1 &&
+      operationSymbols.indexOf(`${temp[len - 1]}`) !== -1
+    ) {
+      let replacedOperator: string;
+      if (len == 2) {
+        replacedOperator = temp[`${len - 1}`];
+      } else {
+        replacedOperator = temp.slice(0, len - 2) + temp[`${len - 1}`];
+      }
+      return replacedOperator;
+    }
+  }
+  return temp;
 };
 
+// Font Size Check Function
 const checkDecreaseFontSize = (
   displayString: string,
   fontSize: number
 ): number => {
   if (displayString.length > maxDigitBeforeSizeChange) {
-      return decreaseFontSize(displayString.length, fontSize);
+    return decreaseFontSize(displayString.length, fontSize);
   } else {
-      return fontSize;
+    return fontSize;
   }
 };
 
@@ -116,88 +141,8 @@ const decreaseFontSize = (stringLength: number, fontSize: number): number => {
   const residualLength = stringLength - maxDigitBeforeSizeChange;
 
   for (let character = 0; character < residualLength; ++character) {
-      fontSize -= fontSize * percentageDecrease;
+    fontSize -= fontSize * percentageDecrease;
   }
 
   return fontSize;
-};
-
-const evaluate = (num1: string, num2: string, operator: string): string => {
-  let evalAnswer: string = num1;
-
-  if (operator) {
-      switch (operator) {
-          case 'add':
-              
-              evalAnswer = String(parseFloat(num1) + parseFloat(num2));
-              break;
-
-          case 'subtract':
-              evalAnswer = String(parseFloat(num1) - parseFloat(num2));
-              break;
-
-          case 'multiply':
-              evalAnswer = String(parseFloat(num1) * parseFloat(num2));
-              break;
-
-          case 'divide':
-              evalAnswer = String(parseFloat(num1) / parseFloat(num2));
-              break;
-
-          case 'modulo':
-              evalAnswer = String(parseInt(num1) % parseInt(num2));
-              break;
-      }
-  }
-
-  return evalAnswer;
-};
-
-const displayResults = (
-  equationField: HTMLInputElement,
-  answerField: HTMLInputElement
-): void => {
-  //Decrease the font size depending on the number of digits
-  if (input[1] === '') {
-      input[1] = '0';
-  }
-  console.log(input[0], input[1]);
-  answer = evaluate(input[0], input[1], operation);
-  const equationFieldString = `${input[0]}${
-      operationDict[<keyof typeof operationDict>operation]
-  }${input[1]}`;
-
-  equationFontSizeChange = checkDecreaseFontSize(
-      equationFieldString,
-      equationOriginalFontSize
-  );
-
-  equationField.style.fontSize = `${equationFontSizeChange}px`;
-  equationField.value = equationFieldString;
-
-  ansFontSizeChange = checkDecreaseFontSize(answer, ansOriginalFontSize);
-
-  answerField.style.fontSize = `${ansFontSizeChange}px`;
-  answerField.value = answer;
-};
-
-//Global Values
-const equationOriginalFontSize = 60;
-const ansOriginalFontSize = 45;
-const maxDigitBeforeSizeChange = 8;
-const percentageDecrease = 0.1;
-
-let input = ['', ''];
-let answer = '';
-let numEquation = 0;
-let operation = '';
-let equationFontSizeChange = equationOriginalFontSize;
-let ansFontSizeChange = ansOriginalFontSize;
-
-const operationDict: Object = {
-  add: '+',
-  subtract: '-',
-  modulo: '%',
-  multiply: 'X',
-  divide: '/',
 };
